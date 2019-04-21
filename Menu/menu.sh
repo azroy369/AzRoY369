d82680a04f451e3574607dc387209792f04a9f40
azroy369 <38562881+azroy369@users.noreply.github.com>
2019-04-22 03:07:14 +0800

    Add files via upload

---
+++ Menu/menu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script by : AzRoY
+clear
+echo -e "\e[93m              AutoScriptVPS by  AzRoY	         "
+echo -e "\e[0m                                                   "
+echo -e "\e[93m             [accounts/options/server]            "
+echo -e "\e[0m                                                   "
